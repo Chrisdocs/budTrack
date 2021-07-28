e3330611e2e77cad6a32efb7423cd5b47abe88c2
Chrisdocs <chrisa.wolfe@gmail.com>
2021-07-28 09:46:32 -0500

    minor

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,12 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const FILES_TO_CACHE = [
-    '/js/idb.js',
-    '/js/index.js',
-    '/index.html',
-    '/css/styles.css'
+    // './icons',
+    './js/idb.js',
+    './js/index.js',
+    './index.html',
+    './css/styles.css',
+    './manifest.json'
 ]
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -36,24 +38,6 @@ self.addEventListener('activate', function(e) {
         })
     );
 });
-
-// self.addEventListener('fetch', function (e) {
-//     console.log('fetch request : ' + e.request.url)
-//     e.respondWith(
-//         caches.match(e.request).then(function (request) {
-//             if (request) { // if cache is available, respond with cache
-//                 console.log('responding with cache : ' + e.request.url)
-//                 return request
-//             } else { // if there are no cache, try fetching request
-//                 console.log('file is not cached, fetching : ' + e.request.url)
-//                 return fetch(e.request)
-//             }
-
-//             // You can omit if/else for console.log & put one line below like this too.
-//             // return request || fetch(e.request)
-//         })
-//     )
-// })
 
 self.addEventListener('fetch', event => {
     // Let the browser do its default thing
